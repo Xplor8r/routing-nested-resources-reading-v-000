8f6797007db099425935b319a62f4cded2476546
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-05 01:45:01 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,9 +5,11 @@ class AuthorsController < ApplicationController
     @posts = @author.posts
     render template: 'posts/index'
   end
-  
+
   def show
     @author = Author.find(params[:id])
+    @post = Post.find(params[:post_id])
+    render template: 'posts/show'
   end
 
 end
